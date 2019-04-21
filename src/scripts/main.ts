4eb2bebc1212395684a0078d9d7de9cf889040b8
Václav Volhejn <vaclav.volhejn@gmail.com>
2019-04-21 16:27:44 +0200

    canvas example

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,4 +1,15 @@
-function displayTotalPerPerson(person: string, total: number) {
-    let message: string = "Total for " + person + " is " + total;
-    document.getElementById("totalMessage").innerText = message;
+var canvas: HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement
+var ctx = canvas.getContext('2d')
+canvas.width = ctx.canvas.clientWidth
+canvas.height = ctx.canvas.clientHeight
+canvas.style.backgroundColor = 'rgb(200,100,100)'
+
+function update() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.fillStyle = 'rgb(200, 200, 200'
+    ctx.fillText('ahdoj', 30, 30)
+
+    window.requestAnimationFrame(update)
 }
+
+window.requestAnimationFrame(update)
